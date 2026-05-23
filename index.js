e99433b83562085c2b6612e1e3bac9fce1bba1bc
CodeByMonir <monirhossen39682@gmail.com>
2026-05-23 23:07:10 +0600

    Jwt is done with lots of error

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ const express = require("express");
 const dotenv = require("dotenv");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const cors = require("cors");
+const { createRemoteJWKSet, jwtVerify } = require("jose-cjs");
 dotenv.config();
 const app = express();
 app.use(cors());
-const port = 4000;
-
-
+const port = process.env.PORT || 4000;
 const uri = process.env.MONGODB_CAR_SERVER;
+
+
+const JWKS = createRemoteJWKSet(new URL(`${process.env.CLIENT_URL}/api/auth/jwks`));
 
 
 const client = new MongoClient(uri, {
@@ -20,9 +22,35 @@ const client = new MongoClient(uri, {
 });
 
 const logger =  (req, res, next) => {
-      console.log(req.params);
+
       next();
     }
+
+const verifyJWT = async (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res.status(401).send("Authorization header is missing");
+  }
+
+  const token = authorization.split(" ")[1];
+  if (!token) {
+    return res.status(401).send("Token is missing");
+  }
+
+  try {
+    const JWKS = createRemoteJWKSet(
+      new URL(`${process.env.CLIENT_URL}/api/auth/jwks`)
+    )
+    const { payload } = await jwtVerify(token, JWKS)
+    req.user = payload;
+
+    
+  } catch (error) {
+    console.error('Token validation failed:', error)
+    throw error
+  }
+  next();
+};
 
 async function run() {
   try {
@@ -31,24 +59,22 @@ async function run() {
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
     const db = client.db("RentACar");
-    const carsCollection = db.collection("cars")
+    const carsCollection = db.collection("cars");
 
-    app.get("/cars", async (req, res)=>{
+    app.get("/cars", async (req, res) => {
       const cursor = carsCollection.find();
       const result = await cursor.toArray();
       res.send(result);
-    })
+    });
 
-    app.get("/cars/:carId", logger,  async (req, res) => {
-        // const carId = req.params.carId;
-        const { carId } = req.params;
+    app.get("/cars/:carId", logger, verifyJWT, async (req, res) => {
+      // const carId = req.params.carId;
+      const { carId } = req.params;
 
-        const query = { _id: new ObjectId(carId) };
-        const result = await carsCollection.findOne(query);
-        res.send(result);
-      },
-    );
-
+      const query = { _id: new ObjectId(carId) };
+      const result = await carsCollection.findOne(query);
+      res.send(result);
+    });
 
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!",
